98310612d8e63c9bcb4da8a48367cf1a6019feac
Francisco Casillas <chiccasillas@gmail.com>
2025-09-10 16:44:17 -0600

    Initial Commit

---
+++ CouplesFinances/CouplesFinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouplesFinancesApp.swift
+//  CouplesFinances
+//
+//  Created by Francisco Jean on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CouplesFinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
